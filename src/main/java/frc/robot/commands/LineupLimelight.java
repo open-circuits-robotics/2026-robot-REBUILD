bf21e0841287fc2406880004386ff17d3b6dcaf5
Computer Jeff <owlrobotics5271@gmail.com>
2026-01-26 16:51:19 -0600

    more unfinished limelight code

--- src/main/java/frc/robot/commands/LineupLimelight.java
+++ src/main/java/frc/robot/commands/LineupLimelight.java
@@ -14,8 +14,11 @@ public class LineupLimelight extends Command {
     protected double pitch, yaw, distToTag;
     protected final double wantedDist = 2;
     protected final double fov = 48.9;
+    public final double  targetHeight = 4;
+    public final double cameraHeight = 2;
     protected boolean hasTarget;
     protected final String name = "limelight-lefty";
+    protected final double targetID = 10;
 
     public LineupLimelight(){
     }
@@ -23,9 +26,17 @@ public class LineupLimelight extends Command {
     @Override
     public void execute(){
         hasTarget = LimelightHelpers.getTV(name);
-        if (hasTarget){
+        if (hasTarget && LimelightHelpers.getFiducialID(name) == targetID){
             double tx = LimelightHelpers.getTX(name);
             double ty = LimelightHelpers.getTY(name);
+            pitch = (ty/2)*fov;
+            yaw = (tx/2)*fov;
+            if (yaw > 0){
+                System.out.println("go left");
+            } else if (yaw < 0){
+                System.out.println("go right");
+            }
+            distToTag = (targetHeight - cameraHeight)/Math.tan(angle + pitch);
         }
     }
 
