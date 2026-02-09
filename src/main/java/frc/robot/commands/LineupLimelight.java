cf83f19d35e2aa57f4ed51e7cd5e4ec3d8634c0a
Computer Jeff <owlrobotics5271@gmail.com>
2026-02-09 15:38:39 -0600

    limelight code theoretically done

--- src/main/java/frc/robot/commands/LineupLimelight.java
+++ src/main/java/frc/robot/commands/LineupLimelight.java
@@ -11,14 +11,22 @@ import frc.robot.LimelightHelpers;
 //For this import to work, make sure the LimelightHelpers.java file is in the java/frc/robot folder, with Constants.java and Main.java and the like
 
 public class LineupLimelight extends Command {
+    //variables to measure what it currently thinks it is at
     protected double pitch, yaw, distToTag;
-    protected final double wantedDist = 2;
-    protected final double fov = 48.9;
-    public final double  targetHeight = 4;
-    public final double cameraHeight = 2;
     protected boolean hasTarget;
-    protected final String name = "limelight-lefty";
-    protected final double targetID = 10;
+
+    //final values which are used to calculate distances.
+    //these should be edited according to robot specifications.
+    protected final double wantedDist = 2; //distance away that you want
+    protected final double vertFOV = 48.9; //vertical camera FOV. For limelight 2, 48.9
+    protected final double horiFOV = 62.5; //horizontal camera FOV. For limelight 2, 62.5
+    protected final double mountingAngleDegrees = 0; //angle the camera is mounted at (unit circle style)
+    public final double  targetHeight = 4; //height of the april tag in question
+    public final double cameraHeight = 2; //height of the camera once mounted on the robot
+    protected final String name = "limelight-lefty"; //name of the camera being used; this is passed into most methods as a parameter
+    protected final double targetID = 10; //id of the target that the camera is meant to look for
+    protected final double acceptableLRRange = 15; //robot will not re-angle if it is facing target april tag within this many degrees
+    protected final double acceptableDistRange = 0.5; //robot will not move forward/backward if it is near wantedDist, within this distance
 
     public LineupLimelight(){
     }
@@ -29,14 +37,19 @@ public class LineupLimelight extends Command {
         if (hasTarget && LimelightHelpers.getFiducialID(name) == targetID){
             double tx = LimelightHelpers.getTX(name);
             double ty = LimelightHelpers.getTY(name);
-            pitch = (ty/2)*fov;
-            yaw = (tx/2)*fov;
-            if (yaw > 0){
+            pitch = (ty/2)*vertFOV;
+            yaw = (tx/2)*horiFOV;
+            if (yaw > 0 + Math.toRadians(acceptableLRRange)){
                 System.out.println("go left");
-            } else if (yaw < 0){
+            } else if (yaw < 0 - Math.toRadians(acceptableLRRange)){
                 System.out.println("go right");
             }
-            distToTag = (targetHeight - cameraHeight)/Math.tan(angle + pitch);
+            distToTag = (targetHeight - cameraHeight)/Math.tan(Math.toRadians(mountingAngleDegrees + pitch));
+            if (distToTag > wantedDist + acceptableDistRange){
+                System.out.println("go forward");
+            } else if (distToTag < wantedDist - acceptableDistRange){
+                System.out.println("go backward");
+            }
         }
     }
 
