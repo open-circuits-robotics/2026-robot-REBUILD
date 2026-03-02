d7ab1c4f27879524205d7e41c3cd5368d884b3cc
Computer Jeff <owlrobotics5271@gmail.com>
2026-03-02 15:53:04 -0600

    remade all that stuff that got lost

--- src/main/java/frc/robot/commands/LineupLimelight.java
+++ src/main/java/frc/robot/commands/LineupLimelight.java
@@ -11,21 +11,35 @@ import frc.robot.subsystems.LimelightSubsystem;
 //For this import to work, make sure the LimelightHelpers.java file is in the java/frc/robot folder, with Constants.java and Main.java and the like
 
 public class LineupLimelight extends Command {
+
+    //do you want it to be precise distance measurements (doesn't work) or relative location measurements (much better)?
+    protected final boolean preciseDist = false;
+
+    //If in relative location mode, are you calibrating? or just fully using it?
+    protected final boolean calibrationMode = true;
+
     //variables to measure what it currently thinks it is at
     protected double pitch, yaw, distToTag;
     protected boolean hasTarget;
 
-    //final values which are used to calculate distances. 
-    //these should be edited according to robot specifications.
+    //These values are only used for the (non functional) precise distance function.
     protected final double wantedDist = 90.0; //distance away that you want
     protected final double vertFOV = 48.9; //vertical camera FOV. For limelight 2, 48.9
     protected final double horiFOV = 62.5; //horizontal camera FOV. For limelight 2, 62.5
     protected final double mountingAngleDegrees = 0; //angle the camera is mounted at (unit circle style)
     public final double  targetHeight = 30.5; //height of the april tag in question
     public final double cameraHeight = 7.5; //height of the camera once mounted on the robot
-    protected final double targetID = 2; //id of the target that the camera is meant to look for
-    protected final double acceptableLRRange = 20; //robot will not re-angle if it is facing target april tag within this many degrees
     protected final double acceptableDistRange = 2; //robot will not move forward/backward if it is near wantedDist, within this distance
+
+    //these vlaues are used only for the relative location function
+    protected final double locX = 0;
+    protected final double locY = 0;
+    protected final double acceptableUDRange = 5;
+
+
+    //these values are used for both that precise distance function and the better relative location function
+    protected final double targetID = 2; //id of the target that the camera is meant to look for
+    protected final double acceptableLRRange = 5; //robot will not re-angle if it is facing target april tag within this many degrees
     
     private final LimelightSubsystem limelightSubsystem; //the subsystem for the command to work with
 
@@ -33,10 +47,45 @@ public class LineupLimelight extends Command {
         this.limelightSubsystem = limelightSubsystem;
         addRequirements(limelightSubsystem);
     }
-    
 
     @Override
     public void execute(){
+        if (preciseDist){
+            usePreciseDistanceMeasurements();
+        } else {
+            useRelativeLocationMeasurements();
+        }
+    }
+
+    public void useRelativeLocationMeasurements(){
+        hasTarget = limelightSubsystem.getTV(); //determines if there is a target on camera
+        if (hasTarget && limelightSubsystem.getFiducialID() == targetID){ //if there is a target on camera, and the target's id number is the correct one, proceed with the code
+            double tx = limelightSubsystem.getTX(); //gets x location of the target on the camera
+            double ty = limelightSubsystem.getTY(); //gets y location of the target on the camera
+            if (calibrationMode){ //if the robot is in calibration mode, prints out the x and y locations of the target on the camera, then ends the method
+                System.out.println("tx: " + tx);
+                System.out.println("ty: " + ty);
+                return;
+            } //otherwise, proceeds to determine for both tx and ty whether they are within range, and gives directions to move accordingly
+            if (tx > locX + Math.toRadians(acceptableLRRange)){
+                System.out.println("go right");
+            } else if (tx < locX - Math.toRadians(acceptableLRRange)){
+                System.out.println("go left");
+            } else {
+                System.out.println("in LR range");
+            }
+            if (ty > locY + acceptableUDRange){
+                System.out.println("go forward");
+            } else if (ty < locY - acceptableUDRange){
+                System.out.println("go backward");
+            } else {
+                System.out.println("in UD range");
+            }
+        }
+    }
+    
+
+    public void usePreciseDistanceMeasurements(){
         hasTarget = limelightSubsystem.getTV(); //determines if there is a target on camera
         if (hasTarget && limelightSubsystem.getFiducialID() == targetID){ //if there is, and it's the right # target, proceed
             double tx = limelightSubsystem.getTX(); //gets x and y offsets of target (i think)
@@ -51,9 +100,9 @@ public class LineupLimelight extends Command {
             distToTag = (targetHeight - cameraHeight)/Math.tan(Math.toRadians(mountingAngleDegrees + pitch)); //calculates horizontal ground distance between target and camera
             distToTag = (targetHeight - cameraHeight)/Math.tan(Math.toRadians(mountingAngleDegrees + ty)); //calculates horizontal ground distance between target and camera
             System.out.println("pitch is " + pitch + ", distance is " + distToTag);
-            if (distToTag > wantedDist + acceptableDistRange){
+            if (distToTag > wantedDist + acceptableUDRange){
                 //System.out.println("go forward");
-            } else if (distToTag < wantedDist - acceptableDistRange){
+            } else if (distToTag < wantedDist - acceptableUDRange){
              //  System.out.println("go backward");
             }
         }
